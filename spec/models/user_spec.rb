f3f26a80f61baae423a8c6676c00d0997729ff7c
Austin Steward <austinsteward@aol.com>
2014-09-14 16:13:35 -0700

    Test user model relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  it { should have_many :posts }
+  it { should have_many :followers }
+  it { should have_many :following }
+  it { should have_many :things }
+
 end
