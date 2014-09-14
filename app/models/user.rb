0afa636910ddd4ac37cfdcbfb31321507727a9e0
Austin Steward <austinsteward@aol.com>
2014-09-14 16:21:14 -0700

    Test user info presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name
   validates_presence_of :handle
+  validates_uniqueness_of :handle
   after_create :welcome_mailer
-  
+
   has_many :followers, :class_name => 'Follower', :foreign_key => 'user_id'
   has_many :following, :class_name => 'Follower', :foreign_key => 'follower_id'
   has_many :posts
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 describe User do
 
   it { should have_many :posts }
+
   it { should have_many :followers }
+
   it { should have_many :following }
+
+  it { should validate_presence_of :name }
+
+  it { should validate_presence_of :handle }
+
+  it { should validate_presence_of :email }
+
+  it { should validate_presence_of :password }
 
 end
