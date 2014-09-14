355257fd54f1ca221684b485ab5b87b585c5e9cd
Austin Steward <austinsteward@aol.com>
2014-09-14 16:15:12 -0700

    Test post model relationships

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Post do
+
+  it { should belong_to :user }
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,5 @@ describe User do
   it { should have_many :posts }
   it { should have_many :followers }
   it { should have_many :following }
-  it { should have_many :things }
 
 end
