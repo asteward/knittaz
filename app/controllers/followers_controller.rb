1aeff50525fd36375ae71c41c26778634d71f1bf
Austin Steward <austinsteward@aol.com>
2014-09-14 15:06:43 -0700

    Add followers controller

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,27 @@
+class FollowersController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @following = @user.followers.new()
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @current_user = current_user
+    @following = @user.followers.new(follower_id: @current_user.id)
+    if @following.save
+      UserMailer.new_follower_notice(@user, current_user).deliver
+      respond_to do |format|
+        format.html { redirect_to user_path(@user) }
+        format.js
+      end
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @follower = @user.followers.find_by(follower_id: current_user.id)
+    @follower.delete
+  end
+end
