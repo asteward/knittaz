908ea5140c0fafc635a8b6dc1bb079cd47191020
Austin Steward <austinsteward@aol.com>
2014-09-14 17:37:48 -0700

    Test navbar user page link

---
+++ spec/features/user_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the sign up process" do
+  before :each do
+    DatabaseCleaner.clean
+    visit '/users/sign_up'
+    test_user = FactoryGirl.create(:user)
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+  end
+
+  it "navigates to the user's page via the navbar" do
+    click_link 'Account'
+    click_link 'My Yarns'
+    expect(page).to have_content '- yarns -'
+  end
+
+end
