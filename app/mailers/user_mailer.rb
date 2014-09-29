d34974aa7b5a5fb4afec2f526a78a26a9e40a17a
Austin Steward <austinsteward@aol.com>
2014-09-28 18:07:40 -0700

    Comment out test mailers for heroku deployment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,19 +10,19 @@ class UserMailer < ActionMailer::Base
     @user = user
     @greeting = "Thanks for signing up, #{@user.name}!"
 
-    mail to: user.email, subject: "Congratulations on becoming a Knittah 4 Life, #{@user.name}!"
+    # mail to: user.email, subject: "Congratulations on becoming a Knittah 4 Life, #{@user.name}!"
   end
 
   def tag_confirmation(user, tagger)
     @user = user
     @greeting = "#{user.name} - You have been stitched by #{tagger.name}!!! MUHUHAHAHAHAHAHAHAHAHAHAHA!!!! >;P"
 
-    mail to: user.email, subject: "You have been stitched!"
+    # mail to: user.email, subject: "You have been stitched!"
   end
 
   def new_follower_notice(user, follower)
     @greeting = "Pleasant greetings, #{user.name}! Just thought you'd like to know that #{follower.name} is now following your yarn! You now have #{user.followers.count} followers!"
 
-    mail to: user.email, subject: "Congrats on your new follower!!!"
+    # mail to: user.email, subject: "Congrats on your new follower!!!"
   end
 end
